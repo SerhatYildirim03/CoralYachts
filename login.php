3711b70e49dff1edeb11d726dd0109c2ed60df57
Justin9701 <126084062+Justin9701@users.noreply.github.com>
2023-12-11 11:16:52 +0100

    updates

--- login.php
+++ login.php
@@ -5,12 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inloggen - Coral Yachts</title>
     <link rel="stylesheet" href="styles/login.css">
-</head>
-<body>
-
-<?php
+    <?php
     include "connect.php";
 ?>
+</head>
+<body>
 
 <header>
     <nav>
