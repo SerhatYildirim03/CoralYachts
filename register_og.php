3efc06f12bd3d1c67cd25e751036f88ef45feb28
Justin9701 <126084062+Justin9701@users.noreply.github.com>
2023-12-19 12:59:55 +0100

    change

--- register_og.php
+++ register_og.php
@@ -10,6 +10,24 @@
 
 <body>
 
+<header>
+    <nav>
+        <div id="logo">
+            <img src="images/jachtlogo.jpg" alt="Coral Yachts Logo" width="150">
+        </div>
+        <ul class="navbar">
+            <li><a href="index.html" class="nav-link">Home</a></li>
+            <li><a href="jachten.html" class="nav-link">Jachten</a></li>
+            <li><a href="reserveren.html" class="nav-link">Reserveren</a></li>
+            <li><a href="contact.html" class="nav-link">Contact</a></li>
+            <li><a href="login.php" class="nav-link">
+                    <img src="images/login-icon.png" alt="Inloggen" class="login-icon">
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
 <?php
 
     // Define variables and initialize with empty values
@@ -170,24 +188,6 @@
     }
 
 ?>
-
-<header>
-    <nav>
-        <div id="logo">
-            <img src="images/jachtlogo.jpg" alt="Coral Yachts Logo" width="150">
-        </div>
-        <ul class="navbar">
-            <li><a href="index.html" class="nav-link">Home</a></li>
-            <li><a href="jachten.html" class="nav-link">Jachten</a></li>
-            <li><a href="reserveren.html" class="nav-link">Reserveren</a></li>
-            <li><a href="contact.html" class="nav-link">Contact</a></li>
-            <li><a href="login.php" class="nav-link">
-                    <img src="images/login-icon.png" alt="Inloggen" class="login-icon">
-                </a>
-            </li>
-        </ul>
-    </nav>
-</header>
 
 <main>
     <section class="register-section">
